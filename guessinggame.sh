8db59cfb9950c7172125bc5d534ed24c20f8c1bc
ksomasundaram27 <krithigasomu@gmail.com>
2021-08-22 22:46:24 -0400

    Updated code

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,26 @@
 #!bash
 
-a=$(pwd)
-echo "Current working directory: $a"
-number_of_files=$(find $a -type f | wc -l)
+#Function to return the path of the current working directory
+function directorypath {
+	a=$(pwd)
+	echo $a
+}
 
+dirpath=$(directorypath)
+filesnum=$(find $dirpath -type f | wc -l)
+echo $dirpath
+
+# Code requesting input from the user for the first time before evaluation
 echo "Please guess the correct number of files"
 read response
 check=0
+
+#Loop logic with conditional check to confirm if the input provided by the user matches the filecount
 while [ $check -ne 1 ]
 do
-	if [ $response -eq $number_of_files ]
+	if [ $response -eq $filesnum ]
 	then
-		echo "Congratulations!! $1 is the correct number of files"
+		echo "Congratulations!! $response is the correct number of files"
 		check=1
 	else
 		echo "Please guess the correct number of files"
